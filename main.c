93e45cb1923721a4d10a5ee5428b51a47800e0e0
Josef Raschen <josef@raschen.org>
2018-03-15 21:32:27 +0100

    Some register defines, set some GPIO B pins as output.

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
+
+#define GPIOB_BASE 0x48000400
+#define GPIO_MODER_OFFSET 0x00 // mode register offset
+#define GPIO_ODR 0x14 // GPIO output data register offset
+#define GPIOB_MODER (GPIOB_BASE + GPIO_MODER_OFFSET) // GPIO B mode register
+#define GPUIB_ODR (GPIOB_BASE + GPIO_ODR) // GPIO B output data register
+
 void clock_init()
 {
-    
+    // just use the default internal RC oscillator for now
+
+
+}
+
+void gpio_init()
+{
+    *(GPIO_B_MODER) |= (0x50000000); // GPIO B14 and B15 are outputs
+
 }
 
 
 int main()
 {
     clock_init();
+
+    gpio_init();
 
     int i;
     while (1) {
