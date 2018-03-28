f909fd6fccce8794c280d6ad919b7be3aff75a1b
Josef Raschen <josef@raschen.org>
2018-03-28 20:30:54 +0200

    HDC1080 seems to needs more time between write and read.

--- hdc1080.c
+++ hdc1080.c
@@ -14,7 +14,7 @@ void hdc_1080_read_temp(struct hdc1080 *h)
     uint8_t data[2] = { HDC1080_REG_TEMP };
 
     i2c_master_write(h->i2c, HDC1080_I2C_ADDR << 1, data, 1);
-    uint32_t cnt = 10000;
+    uint32_t cnt = 100000;
     while (cnt-- > 0);
     i2c_master_read(h->i2c, HDC1080_I2C_ADDR << 1, data, 2);
 
@@ -27,7 +27,7 @@ void hdc_1080_read_humidity(struct hdc1080 *h)
     uint8_t data[2] = { HDC1080_REG_HUM };
 
     i2c_master_write(h->i2c, HDC1080_I2C_ADDR << 1, data, 1);
-    uint32_t cnt = 10000;
+    uint32_t cnt = 100000;
     while (cnt-- > 0);
     i2c_master_read(h->i2c, HDC1080_I2C_ADDR << 1, data, 2);
 
